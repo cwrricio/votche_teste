7ef5853c44a2db607b2095c8791d661e798b9993
Emanuel Carricio <emanuelferreira.aluno@unipampa.edu.br>
2025-09-03 23:21:30 -0300

    Atualizações no front fix 1

--- votche-front/src/App.jsx
+++ votche-front/src/App.jsx
@@ -31,7 +31,7 @@ import EnterMeeting from "./components/EnterMeeting";
 import MeetingSession from "./components/MeetingSession";
 import ArchivedMeetings from "./components/ArchivedMeetings";
 import Sidebar from "./components/Sidebar";
-import Topbar from '@/components/Topbar.jsx';
+import Topbar from '@/components/TopBar';
 import ReportDashboard from "./components/ReportDashboard";
 
 
